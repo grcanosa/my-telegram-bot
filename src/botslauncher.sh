92f3763141171c0c650b735fb9efce0f6cf423a9
grcanosa <grcanosa@gmail.com>
2016-11-14 15:05:32 +0100

    Modified launcher.sh to launch multiple bots

---
+++ src/botslauncher.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Put this file in /usr/local/etc/rc.d/btsync.sh
+
+case "$1" in
+
+stop)
+    echo "Stop Bots"
+    kill "`cat /tmp/telegrambots.pid`"
+    kill "`cat /tmp/telegrambots.pid`"
+    ;;
+start)
+    ./botslauncher.py &
+    echo $! > /tmp/telegrambots.pid
+    ;;
+restart)
+    $0 stop
+    sleep 1
+    $0 start
+    ;;
+*)
+    echo "usage: $0 { start | stop | restart }" >&2
+        exit 1
+        ;;
+esac
